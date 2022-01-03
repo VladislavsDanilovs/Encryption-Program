2fde11562761a680087b50bc1a9e65620a422f0e
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-01-03 14:48:12 +0200

    fix encryption program

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-//package Lesson0_PracticeGUI_Encryption;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        EncryptionProgram encryptionProgram = new EncryptionProgram();
-//
-//    }
-//}
+package Lesson0_PracticeGUI_Encryption;
+
+public class Main {
+   public static void main(String[] args) {
+       EncryptionProgram encryptionProgram = new EncryptionProgram();
+
+   }
+}
