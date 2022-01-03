2bfec0d60597cf32fe5cfebdef358bdd4e19a3f4
Vlad Danilov <vladislavs.danilovs@inbox.lv>
2022-01-03 14:46:45 +0200

    add encryption program

---
+++ Main.java
@@ -0,0 +1,8 @@
+//package Lesson0_PracticeGUI_Encryption;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        EncryptionProgram encryptionProgram = new EncryptionProgram();
+//
+//    }
+//}
